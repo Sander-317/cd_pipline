00b822a6bd80311113c2ca09e85f450a2da9243e
Sander-317 <sander.van.meerveld@gmail.com>
2023-11-20 11:46:47 +0100

    changed the .sh file on server

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 cd /home/cd_pipline
 git pull origin main
 pip install -r requirements.txt
-systemctl restart gunicorn
+systemctl restart farm.service
 # pkill gunicorn
 # gunicorn main:app --bind 0.0.0.0
