5c8df507f4287e27bcfc23d64163db4fd10077b1
Sander-317 <sander.van.meerveld@gmail.com>
2023-11-20 14:10:30 +0100

    test adding new packeges to project

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Import what we need from flask
 from flask import Flask
+import os
+import pyjokes
+import subprocess
 
 # Create a Flask app inside `app`
 app = Flask(__name__)
@@ -7,7 +10,8 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return '<h1>welcome to my flask app that gets deployed by github actions</h1> <img src="https://github.com/Sander-317/cd_pipline/actions/workflows/run-test.yml/badge.svg" alt="badge">'
+    result = subprocess.run(["pyjoke"], stdout=subprocess.PIPE)
+    return f'<h1>welcome to my flask app that gets deployed by github actions</h1> <img src="https://github.com/Sander-317/cd_pipline/actions/workflows/run-test.yml/badge.svg" alt="badge"> <div><h1>Joke<h1><br><h2>{result.stdout[:-2]}<h2></div>'
 
 @app.route('/test')
 def cow():
