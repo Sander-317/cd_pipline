923a3ee167fb6d187601f89af9cb93caeef9066f
Sander-317 <sander.van.meerveld@gmail.com>
2023-11-20 12:03:14 +0100

    added systemctl status farm.service

--- test.sh
+++ test.sh
@@ -2,5 +2,6 @@ cd /home/cd_pipline
 git pull origin main
 pip install -r requirements.txt
 systemctl restart farm.service
+systemctl status farm.service
 # pkill gunicorn
 # gunicorn main:app --bind 0.0.0.0
