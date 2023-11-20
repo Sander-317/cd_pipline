01f855ff42556e40ddcc8b7a5a91e6a0204b9e5e
Sander-317 <sander.van.meerveld@gmail.com>
2023-11-20 11:32:40 +0100

    added deplyment workflow shoud work test

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 cd /home/cd_pipline
 git pull origin main
 pip install -r requirements.txt
-pkill gunicorn
-gunicorn main:app --bind 0.0.0.0
+systemctl restart gunicorn
+# pkill gunicorn
+# gunicorn main:app --bind 0.0.0.0
