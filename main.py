3965dc979c7e3bb753c9a058550d14eb8e2815f1
Sander-317 <sander.van.meerveld@gmail.com>
2023-11-20 15:22:27 +0100

    made everithing look nice

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-# Import what we need from flask
 from flask import Flask
 import subprocess
 
-# Create a Flask app inside `app`
+
 app = Flask(__name__)
 
-# Assign a function to be called when the path `/` is requested
+
 @app.route('/')
 def index():
     result = subprocess.run(["pyjoke"], stdout=subprocess.PIPE)
-    return f'<h1>welcome to my flask app that gets deployed by github actions</h1> <img src="https://github.com/Sander-317/cd_pipline/actions/workflows/run-test.yml/badge.svg" alt="badge"> <div><h1>Joke<h1><h2>{result.stdout.decode("utf-8")}<h2><h3>hit refresh to refresh the joke</h3></div>'
+    number_of_jokes = "\"number of jokes\""
+    return f'<h1>welcome to my flask app that gets deployed by github actions</h1> <img src="https://github.com/Sander-317/cd_pipline/actions/workflows/run-test.yml/badge.svg" alt="badge"> <div><h1>Joke<h1><h2>{result.stdout.decode("utf-8")}<h2><h3>hit refresh to refresh the joke</h3><h3>if you need more jokes go to http://157.230.19.4/jokes/{number_of_jokes} and you get all the jokes you want  </div>'
 
-@app.route('/test')
-def cow():
-    return 'it still works test again'
+@app.route('/jokes/<number>')
+def get_jokes(number):
+    try:
+        joke_list = []
+        for i in range(int(number)):
+             result = subprocess.run(["pyjoke"], stdout=subprocess.PIPE)
+             joke_list.append(result.stdout.decode("utf-8"))
+    except:
+        return "<h1>the end of the url must be a number</h1>"
 
+    test = [f"{joke}" for joke in joke_list ]
+    
+    return f'<ol><h1>{number} jokes list</h1>{[f"<li><h2>{joke[:-2]}<h2> </li> " for joke in joke_list ]}</ol>'
+    
 
 if __name__ == '__main__':
     app.run( "0.0.0.0", debug=True)
